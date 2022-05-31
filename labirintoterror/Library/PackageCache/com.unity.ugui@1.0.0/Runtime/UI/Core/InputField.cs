8547ef125c8a9bc602c064e8725524b56184072e
LucasWey <101645516+LucasWey@users.noreply.github.com>
2022-05-31 10:59:20 -0300

    1.2

--- labirintoterror/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
+++ labirintoterror/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
@@ -1144,7 +1144,7 @@ namespace UnityEngine.UI
                 m_TextComponent.UnregisterDirtyVerticesCallback(UpdateLabel);
                 m_TextComponent.UnregisterDirtyMaterialCallback(UpdateCaretMaterial);
             }
-            CanvasUpdateRegistry.DisableCanvasElementForRebuild(this);
+            CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
 
             // Clear needs to be called otherwise sync never happens as the object is disabled.
             if (m_CachedInputRenderer != null)
@@ -1155,12 +1155,6 @@ namespace UnityEngine.UI
             m_Mesh = null;
 
             base.OnDisable();
-        }
-
-        protected override void OnDestroy()
-        {
-            CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
-            base.OnDestroy();
         }
 
         IEnumerator CaretBlink()
@@ -1337,7 +1331,6 @@ namespace UnityEngine.UI
             switch (platform)
             {
                 case RuntimePlatform.Android:
-                case RuntimePlatform.WebGLPlayer:
                     return !TouchScreenKeyboard.isInPlaceEditingAllowed;
                 default:
                     return TouchScreenKeyboard.isSupported;
@@ -2464,11 +2457,6 @@ namespace UnityEngine.UI
                     m_DrawEnd = m_Text.Length;
                 }
 
-                // To fix case 1320719; we need to rebuild the layout before we check the number of characters that can fit within the extents.
-                // Otherwise, the extents provided may not be good.
-                textComponent.SetLayoutDirty();
-                Canvas.ForceUpdateCanvases();
-
                 if (!isEmpty)
                 {
                     // Determine what will actually fit into the given line
@@ -3352,7 +3340,7 @@ namespace UnityEngine.UI
         /// <summary>
         /// See ILayoutElement.minWidth.
         /// </summary>
-        public virtual float minWidth { get { return 5; } }
+        public virtual float minWidth { get { return 0; } }
 
         /// <summary>
         /// Get the displayed with of all input characters.
